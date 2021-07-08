1148e4ae13c1354a83fd3046d36ff24729fc3ec2
Milos-11 <mlopezquinta@yahoo.es>
2021-07-08 19:36:40 +0200

    Comienzo del proyecto

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Documento sin título</title>
+    <style>
+        .resaltar{
+            color:brown;
+            font-weight: bold;
+
+        }
+        </style>
+		
+		<link href="css/new 2.css" rel="stylesheet" type="text/css">
+        </head>
+        <body>
+            <?php
+            echo "<p class='resaltar'>Esto es un ejemplo de frase</p>"
+
+            ?>
+        </body>
+		<h1>Proyecto empresarial</h1>
+		<h2> Nuevo rotulo </h2>
+		<h1> Rotulo 3</h1>
+        <html>
